da7d209c4307bcc3ef1b609d81f939cab2025f76
Julio <julio.mech@hotmail.com>
2023-05-21 08:43:05 -0300

    Set useState buttons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Formulario from './Formp';
 import Tabela from './Tablep';
+import { useState } from 'react';
 
 function App() {
+
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+
   return (
     <div>
-      <Formulario />
+      <Formulario botao={btnCadastrar} />
       <Tabela />
     </div>
   );
